c0e4bf786b836f766b99f25cd881f49e502a2d8c
John Tran <john.jvtran@gmail.com>
2020-07-19 21:19:09 -0500

    transfer website files

---
+++ lib/sections/simplesteps.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+import '../widget/flatbutton.dart';
+
+Widget buildSimpleSteps(BuildContext context) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Expanded(
+        flex: 4,
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Simple steps to get started'.toUpperCase(),
+              style: TextStyle(fontSize: 26),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              // color: Colors.amber,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(),
+                    flex: 2,
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Provide Location",
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Comment",
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Give",
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.play_arrow,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Feel Good",
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            buildAskButton(context, 'Get AskGiveLocally Free')
+          ],
+        ),
+      ),
+      Expanded(
+        flex: 4,
+        child: Image.asset(
+          'images/asl_screens.png',
+          // fit: BoxFit.fill,
+        ),
+      ),
+    ],
+  );
+}
